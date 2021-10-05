263106a9274eb2e606119aa57149bae65e95b2cc
Bruno Magalhães <brunofellipe20@gmail.com>
2021-10-05 02:33:48 -0300

    refact integration controller ok

--- src/controllers/integrationController.js
+++ src/controllers/integrationController.js
@@ -1,33 +1,24 @@
 const { response } = require("express");
-const axios = require("axios");
-const Balance = require("../models/Balance");
-const pipedrive_api_key = process.env.PIPEDRIVE_API_KEY;
 
-const {
-  won_deals,
-  save_orders,
-  listaOrders,
-  check_before_save,
-  listaOrdersValor,
-} = require("../utils/integrations.utils");
+const { won_deals, save_orders } = require("../utils/integrations.utils");
 
 require("dotenv").config();
 
 module.exports = {
-  async Integrate(request, response) {
+  async registerDealAsOrder(request, response) {
     try {
-      let a = await won_deals();
-      return response.status(200).send(a);
+      let deals = await won_deals();
+      return response.status(200).send(deals);
     } catch (error) {
       console.log(error);
-      return response.status(400).send({ error: " erro na requisição" });
+      return response.status(400);
     }
   },
 
-  async save(request, response) {
+  async saveOrders(request, response) {
     try {
-      let teste = await save_orders();
-      return response.status(200).send(teste);
+      let saved_orders = await save_orders();
+      return response.status(200).send(saved_orders);
     } catch (error) {
       return response.status(400).send({ error: " erro na requisição" });
     }
